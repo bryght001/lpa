cdaa1fe8e6ef3782aaca0d98c023aee3d85613b4
bryght001 <brightchukwunwike@gmail.com>
2024-11-22 12:43:20 +0100

    lpa website

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a projects for let's plan africa.
+This is a free project not paid.
+
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en_US">
+<meta name ="viewport" content="width=device-width,initial-scale=1.0"/>
+<meta name ="Author" content ="biggbryte"/>
+<meta name ="Description" content ="Let's Plan Africa NGO" />
+<head>
+<title>Let's Plan Africa </title>
+<link rel ="stylesheet" href="assets/css/main.css"/>
+
+</head>
+
+<body>
+
+
+
+</body>
+
+</html>
